579defc2409f77c5068d77223f6b948c06d9be5f
kjstarun <kjstarun@gmail.com>
2023-03-21 15:54:57 +0530

    added with validation schema for users model

--- Evernote/src/api/Users/user.model.js
+++ Evernote/src/api/Users/user.model.js
@@ -2,10 +2,26 @@ import Mongoose from "mongoose";
 
 const UserSchema = new Mongoose.Schema(
   {
-    userName: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
-    contactNumber: { type: String, required: true },
+    userName: {
+      type: String,
+      required: true,
+      validate: /^[a-zA-Z0-9_-]{3,16}$/,
+    },
+    email: {
+      type: String,
+      required: true,
+      validate: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/,
+    },
+    contactNumber: {
+      type: String,
+      required: true,
+      validate: /^\+[0-9]{1,3}[0-9]{3,14}$/,
+    },
     isActive: { type: Boolean, required: true },
   },
   { timestamps: true }
